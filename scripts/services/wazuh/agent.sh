60b50d07ad129641cc30dfcc7f1c7544e71684fd
Steven Foerster <steven@cyber5k.com>
2021-05-02 20:41:19 -0400

    do not append wazuh list

--- scripts/services/wazuh/agent.sh
+++ scripts/services/wazuh/agent.sh
@@ -9,7 +9,7 @@ fi
 # prepare repo
 echo "Adding Wazuh Repository"
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
-echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee /etc/apt/sources.list.d/wazuh.list
 
 apt-get update
 
