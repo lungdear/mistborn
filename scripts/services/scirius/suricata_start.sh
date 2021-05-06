025a0840a0b67fb45d5848b60e2681f48b39da2e
Steven Foerster <steven@cyber5k.com>
2021-05-06 09:06:24 -0400

    suri variables

--- scripts/services/scirius/suricata_start.sh
+++ scripts/services/scirius/suricata_start.sh
@@ -4,4 +4,4 @@ systemctl start suricata
 systemctl enable suricata
 
 apt-get install -y python-pyinotify python-daemon
-python /opt/mistborn/scripts/services/scirius/suri_reloader &
+python /opt/mistborn/scripts/services/scirius/suri_reloader -p /etc/suricata/rules -D
