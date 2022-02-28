8485c8b5a82a51199ffc7015b2b6504fd1c4e84a
Steven Foerster <steven@cyber5k.com>
2022-02-28 10:32:54 -0500

    syntax

--- scripts/env/migrations/run_migrations.sh
+++ scripts/env/migrations/run_migrations.sh
@@ -24,7 +24,7 @@ mistborn_add2file() {
     elif grep -q -e "${preceding_string}" "${target_filename}"; then
 
         # add after given line
-        sudo sed -i "s/.*${preceding_string}.*/&\n${target_string}" "${target_filename}"
+        sudo sed -i "s/.*${preceding_string}.*/&\n${target_string}/" "${target_filename}"
 
     else
         # add to bottom of file
