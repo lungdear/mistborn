72bf3fadd35cfbe27a60eb1c13dc6d430e4b8f06
Steven Foerster <steven@cyber5k.com>
2022-03-01 23:04:48 -0500

    syntax

--- scripts/env/setup.sh
+++ scripts/env/setup.sh
@@ -49,7 +49,7 @@ sudo sed -i "s/#name_servers.*/name_servers=${MISTBORN_BIND_IP}/" /etc/resolvcon
 sudo sed -i "s/name_servers.*/name_servers=${MISTBORN_BIND_IP}/" /etc/resolvconf.conf
 
 # MISTBORN_ACCESS_RULE
-echo "MISTBORN_ACCESS_RULE=\"ClientIP(\`10.0.0.0/8\`, \`192.168.0.0/16\`, \`172.16.0.0/12\`, \`127.0.0.0/8\`)\"" | sudo tee -a ${VAR_FILE}
+echo "MISTBORN_ACCESS_RULE=ClientIP(\`10.0.0.0/8\`, \`192.168.0.0/16\`, \`172.16.0.0/12\`, \`127.0.0.0/8\`)" | sudo tee -a ${VAR_FILE}
 
 # MISTBORN_TAG
 
